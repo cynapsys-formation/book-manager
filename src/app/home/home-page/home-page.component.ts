8ab09fb16082801b84a3085db5cfb3f739f938df
wajdi ben haj hassen <w.belhajhassen@cynpasys.de>
2018-04-24 11:48:07 +0200

    Add bootstrap

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    this.activeRoute.params.subscribe((params) => {
+      console.log('id=', params['id']);
+    });
+
   }
 
 }
